6204048ff984d13ee8accc62b97ab01033beb43c
Pablo Carmona <lpablouc9@gmail.com>
2023-04-05 16:39:20 -0600

    Print data

--- receptor/receptor.c
+++ receptor/receptor.c
@@ -1,4 +1,4 @@
-#include "structures.c"
+#include "../common/structures.c"
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,10 +15,7 @@ struct shm_caracter Create_caracter(int dir){
     srand(time(NULL));
 
     caracter.value= rand() % (127 + 1 - 33) + 33;
-    printf("Original value: %d\n", caracter.value);
-    caracter.value=caracter.value^70;
-    printf("XOR value: %d\n", caracter.value);
-    
+    caracter.value=caracter.value^70;    
     
     caracter.datetime= *localtime(&current_time);
     //caracter.buffer_idx= rand() % 300;
@@ -46,49 +43,122 @@ int writeFile(int fd, off_t offset, char value){
 
     char* str = &value;
     size_t len = sizeof(*str);
-    printf("size of str %zu\n", len);
     if (write(fd, str, len) != len) {
         perror("Error writing to file");
         close(fd);
         return 1;
     }
 
+    return 0;
+}
+
+
+void chooseColor(){
+    srand(time(NULL));
+    int receptorNum= rand() % 6;
+
+    switch(receptorNum){
+        case 0:// Set console text color to red
+            printf("\033[31m");
+            break;
+
+        case 1:// Set console text color to magenta
+            printf("\033[35m");
+
+        case 2:// Set console text color to blue
+            printf("\033[34m");
+            break;
+
+        case 3:// Set console text color to yellow
+            printf("\033[33m");
+
+        case 4:// Set console text color to green
+            printf("\033[32m");
+            break;
+
+        case 5:// Set console text color to cyan
+            printf("\033[36m");
+            break;
+    }
+}
+
+void printCharacter(struct shm_caracter character, char asciiValue){
+    char datetime_str[50];
+    strftime(datetime_str, sizeof(datetime_str), "%c", &character.datetime);
+
+    printf("Caracter leido \n");
+
+    // Print table
+    printf("| %-20s | %-15c \n", "Valor del caracter", asciiValue);
+    printf("| %-20s | %-15d \n", "Indice de lectura", character.buffer_idx);//Posicion del buffer o del file?
+    printf("| %-20s | %-15s \n", "Fecha y hora", datetime_str);
+}
+
+
+int printText(){
+    int fd = open("data.txt", O_RDONLY);
+     if (fd == -1) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    char buffer[100];
+    ssize_t n = read(fd, buffer, 100);
+
+    if (n == -1) {
+        printf("Failed to read from file\n");
+        return 1;
+    }
+
+    printf("File contents:\n");
+    printf("%.*s", (int)n, buffer);
+    printf("\n");
+    
     // close the file descriptor when we are finished with it
     close(fd);
+
     return 0;
 }
 
 
 void receptor(int mode,  int key){
-    char asciiValue;
-    int getValue;
 
-    struct shm_caracter c1= Create_caracter(2);
-    //Make XOR to decrypt
-    getValue= c1.value ^ key;
-    //convert value to ascii
-    printf("Pre--- value %d\n", getValue);
-    asciiValue = (char) getValue;
-    printf("ASCII value %c\n", asciiValue);
-    int fd = getFile();
-    off_t offset = c1.file_idx;
-    writeFile(fd, offset , asciiValue);
+    if(mode == 0){ 
+        //Add logic to make it automatic
+    }
+    else{
+        while(1){//Adjust the condition. Needs to be cycled until there is a signal.
+            while(1){
+                printf("Press Enter to execute the function.\n");
+                int key= getchar();
+                if(key==10){
+                    break;
+                } 
+            }
+            char asciiValue;
+            int getValue;
+
+            struct shm_caracter c1= Create_caracter(2);
+            //Make XOR to decrypt
+            getValue= c1.value ^ key;
+            //convert value to ascii
+            //////printf("Pre--- value %d\n", getValue);
+            asciiValue = (char) getValue;
+            /////printf("ASCII value %c\n", asciiValue);
+            int fd = getFile();
+            off_t offset = c1.file_idx;
+            writeFile(fd, offset , asciiValue);
+            printCharacter(c1, asciiValue);
+            printText();
+        }
+    }   
 
 }
 
 int main(){
-
-    /* struct shm_caracter c1= Create_caracter(1);
-    printf("Value: %d\n", c1.value);
-    printf("Buffer index: %d\n",c1.buffer_idx);
-    printf("File index: %d\n",c1.file_idx);
-    
-    char datetime_str[50];
-    strftime(datetime_str, sizeof(datetime_str), "%c", &c1.datetime);
-    printf("Datetime: %s\n", datetime_str); */
-
-    receptor(0, 70);
-
+    chooseColor();
+    receptor(1, 70);
 
     return 0;
 }
+
