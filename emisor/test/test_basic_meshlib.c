2fb3dee0cfbaf48e8762aa9a5384f24b5c7988ca
kev <kzeledon@pm.me>
2023-04-06 12:57:04 -0600

    Correctly linking

--- emisor/test/test_basic_meshlib.c
+++ emisor/test/test_basic_meshlib.c
@@ -8,6 +8,25 @@
 
 #include "mesh_emitter.h"
 
+void * shm_ptr;
+int buffer_size;
+
+void first_setup() {
+    buffer_size = 13;
+    //mesh_initialize(buffer_size);
+    shm_ptr = mesh_get_shm_ptr();
+}
+
+void last_teardown() {
+    mesh_finalize(shm_ptr);
+}
+
 int main () {
+    printf("### Running basic emitter meshlib tests!\n\n");    
+    first_setup();
+
+    
+
+    last_teardown();
     return 0;
 }
