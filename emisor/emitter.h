60981e85ad8221f94834acf98638374c0a045e41
Jonathana1106 <jonathana1196@gmail.com>
2023-04-10 19:01:54 -0600

    [ADD] documentation

--- emisor/emitter.h
+++ emisor/emitter.h
@@ -1,6 +1,21 @@
 #ifndef EMITTER
 #define EMITTER
 
+/*
+Este programa utiliza las siguientes bibliotecas:
+
+- stdio.h: Proporciona funciones para entrada y salida estándar.
+- stdlib.h: Proporciona funciones para la gestión de memoria dinámica.
+- string.h: Proporciona funciones para la manipulación de cadenas.
+- unistd.h: Proporciona funciones para el acceso a recursos del sistema operativo.
+- sys/shm.h: Proporciona funciones para la gestión de memoria compartida.
+- sys/ipc.h: Proporciona funciones para la gestión de claves de IPC.
+- semaphore.h: Proporciona funciones para el manejo de semáforos.
+- fcntl.h: Proporciona funciones para el manejo de archivos.
+- errno.h: Define una variable externa errno que se utiliza para almacenar información sobre errores.
+- time.h: Proporciona funciones para trabajar con fechas y horas.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,11 +27,27 @@
 #include <errno.h>
 #include <time.h>
 
+/*
+Este programa utiliza las siguientes definiciones:
+
+- MAX_BUFFER_SIZE: Define el tamaño máximo del búfer utilizado para leer y escribir en la memoria compartida.
+- SHM_KEY: Define la clave utilizada para la memoria compartida.
+- SEM_KEY: Define la clave utilizada para el semáforo.
+- SHM_SIZE: Define el tamaño de la memoria compartida en bytes.
+- SEMAPHORE_NAME: Define el nombre del semáforo utilizado en el programa.
+*/
+
 #define MAX_BUFFER_SIZE 1024
 #define SHM_KEY 0x1234
 #define SEM_KEY 0x5678
 #define SHM_SIZE 1024
 #define SEMAPHORE_NAME "/mesh_semaphore"
+
+/*
+ * Struct que representa un caracter en la memoria compartida.
+ * Contiene el valor del caracter, su índice de posición en la memoria
+ * compartida, y la hora en la que fue introducido.
+ */
 
 struct shm_caracter {
     char value;
